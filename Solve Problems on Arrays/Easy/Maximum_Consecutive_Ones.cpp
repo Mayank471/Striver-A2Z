5e0b791b00e498a1e98e47bfb6eeb22baf4eee13
Mayank <mayankdahiya8431@gmail.com>
2025-05-24 18:08:53 +0530

    Questions 24 May, 25

---
+++ Solve Problems on Arrays/Easy/Maximum_Consecutive_Ones.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int count = 0 , maxcount = 0;
+        for(int i = 0 ; i<nums.size();i++) {
+            if(nums[i] == 1)  {
+                count++;
+                if(count > maxcount) maxcount = count;
+            }
+            else count = 0;
+        }
+        return maxcount;
+    }
+};
