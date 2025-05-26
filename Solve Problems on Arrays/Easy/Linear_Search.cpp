96ad6cedc42f6d10bdfc083973ea3a4d0d90b77d
Mayank <mayankdahiya8431@gmail.com>
2025-05-26 11:27:20 +0530

    Questions 26 May , 25

---
+++ Solve Problems on Arrays/Easy/Linear_Search.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+    // Function to find element in sorted array
+    // arr: input array
+    // k: element to be searched
+    bool searchInSorted(vector<int>& arr, int k) {
+        bool state = false;
+        // Your code here
+        for(int i = 0 ; i < arr.size();i++) {
+            if(arr[i]==k)state = true;
+        }
+        return state;
+    }
+};
