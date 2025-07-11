769610a39cdf485a8383d241364a344135255d3b
Mayank <mayankdahiya8431@gmail.com>
2025-07-12 03:42:20 +0530

    Questions 12 July 25

---
+++ Learn LinkedList/Medium Problems of LL/Middle_of_LinkedList_TortoiseHare_Method.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        int len = 0;
+        ListNode* curr = head;
+        while(curr!=NULL) {
+            len++;
+            curr=curr->next;
+        }
+        for( int i = 0 ; i < len/2; i++) {
+            head = head->next;
+        }
+        return head;
+    }
+};
